0b624755372e956901b0db6f6e7ec46d98092964
Hugo <hugo_m7@live.fr>
2023-10-07 15:54:38 +0200

    MAJ

--- js/main.js
+++ js/main.js
@@ -2,36 +2,47 @@
 
 "use strict";
 
+// import haversine from "/math.js"
+
 const elements = {
     list: document.querySelector('#cine_list')
 }
 
 const url = `https://data.culture.gouv.fr/api/explore/v2.1/catalog/datasets/etablissements-cinematographiques/records?limit=20`;
 
-
-
-//recup loca user
-onload = () => {
-    console.log("Loaded");
-  };
-
-
 // sort cinemas 
-fetch(url).then(response => response.json()).then(response => {
+onload = () => {
 
-    let outputHTML = "";
-    response.results.sort((a, b) => b.fauteuils - a.fauteuils).forEach(cinema => {
-        outputHTML += `
-            <tr>
-                <td>${cinema.nom}</td>
-                <td>${cinema.adresse}</td>
-                <td>${cinema.commune}</td>
-            </tr>
-        ` 
-        elements.list.innerHTML = outputHTML;
 
-    })
+    // get geoloc info
+    let location;
+    navigator.geolocation.getCurrentPosition(position => {
+        
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        location = [latitude, longitude];
+        console.log(location);
 
-});
+    });
 
-//
+    // display cinemas
+    fetch(url).then(response => response.json()).then(response => {
+
+        let outputHTML = "";
+
+        response.results.sort((a, b) => b.fauteuils - a.fauteuils).forEach(cinema => {
+            outputHTML += `
+                <tr>
+                    <td>${cinema.nom}</td>
+                    <td>${cinema.adresse}</td>
+                    <td>${cinema.commune}</td>
+                </tr>
+            ` 
+            elements.list.innerHTML = outputHTML;
+
+        })
+
+    });
+
+};
+
